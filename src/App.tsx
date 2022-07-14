bc89859e51fbbc0a2ed5d5b856386d8d0c9b5a5b
Mustafa <mustafaanvarov1@gmail.com>
2022-07-14 18:10:03 +0500

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReminderList from './Components/ReminderList';
+import Reminder from './models/reminder';
+
+const reminders: Reminder[] = [
+  {id: 1, title: 'Reminder 1'}
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReminderList items={reminders}/>
+      <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A9208206187206f7ec532855c9688a738a9aa380ef14b21472a7d5e9021127f04&amp;source=constructor" width="100%" height="720" frameBorder="0"></iframe>
+
     </div>
   );
 }
